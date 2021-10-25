0b67bddcfb2406f33869efc5783f3fe1317ea093
Diego Perez <diegopdc15@gmail.com>
2021-10-24 23:29:53 -0300

    cambio prueba1

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/processSA'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/processSA/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
